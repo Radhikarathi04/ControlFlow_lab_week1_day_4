398729b80e9474406fabcd37e8946b9d08928f59
Radhika <radhikarathi308@gmail.com>
2022-11-10 19:12:58 +0000

    guessing game and weather

--- ClothingWeatherGame/src/main/java/WhatShouldIWear.java
+++ ClothingWeatherGame/src/main/java/WhatShouldIWear.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class WhatShouldIWear {
     public static void main(String[] args) {
+
+        // prompt the user for input about the weather
+        System.out.println("What's the weather like today?");
+
+        // collect user input about the weather and temperature that day
+        Scanner reader = new Scanner(System.in);
 
     }
 }
