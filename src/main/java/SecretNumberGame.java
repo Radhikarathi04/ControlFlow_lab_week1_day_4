0c29fd5b0a05a8cf7d076f4d1ca328503e0b3c29
Radhika <radhikarathi308@gmail.com>
2022-11-10 16:55:58 +0000

    adds guessing game

---
+++ src/main/java/SecretNumberGame.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SecretNumberGame {
+    public static void main(String[] args) {
+
+        //prompt the user for input to enter a number
+        System.out.println("Guess the secret number");
+
+        // collecting users guess
+        
+    }
+}
