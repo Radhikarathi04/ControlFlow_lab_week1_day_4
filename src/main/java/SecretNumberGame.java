d6a0da7ed24377b837d12999a61184af8bf77bac
Radhika <radhikarathi308@gmail.com>
2022-11-10 17:57:34 +0000

    MVP GuessingNumberGame

--- src/main/java/SecretNumberGame.java
+++ src/main/java/SecretNumberGame.java
@@ -3,10 +3,28 @@ import java.util.Scanner;
 public class SecretNumberGame {
     public static void main(String[] args) {
 
-        //prompt the user for input to enter a number
-        System.out.println("Guess the secret number");
+        //prompt the user for input to guess a number
+        System.out.println("Guess a number from 1 to 10");
 
-        // collecting users guess
-        
+        // collect user input
+        Scanner reader = new Scanner(System.in);
+        int guessNumber = reader.nextInt();
+
+        System.out.println("You guessed a number: " + guessNumber);
+
+        // Secret Number
+        int secretNumber = 4;
+
+        // if guessNumber = 4, print "Yay, you won!"
+        // else print "try again"
+
+        if (guessNumber == 4) {
+            System.out.println("Yay, you got it correct!");
+        } else if (guessNumber < secretNumber) {
+            System.out.println("Too low. Try again");
+        }
+        else if (guessNumber > secretNumber){
+            System.out.println("Too high. Try again");
+        }
     }
 }
